4882346a2c1b168396ed74f0cdc5e050d6aeb496
Ali <beingalisoomro@gmail.com>
2021-08-06 16:09:04 -0700

    Added child model

--- app/src/main/java/com/example/e_vaccination/models/Child.java
+++ app/src/main/java/com/example/e_vaccination/models/Child.java
@@ -1,4 +1,57 @@
 package com.example.e_vaccination.models;
 
-public class Childern {
+public class Child {
+    private String name;
+    private String dob;
+    private String image;
+    private String key;
+    private String gender;
+
+    public Child(String name, String dob, String image, String key, String gender) {
+        this.name = name;
+        this.dob = dob;
+        this.gender = gender;
+        this.image = image;
+        this.key = key;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
 }
